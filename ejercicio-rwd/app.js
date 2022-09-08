4bcf52f0f11ebf55183aed433c85e1b8efa68a07
AguAF <agustin.alonso@cencosud.com>
2022-09-08 11:43:53 -0300

    .

--- ejercicio-rwd/app.js
+++ ejercicio-rwd/app.js
@@ -1,15 +1,14 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = 4000
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 4000;
 
+app.use(express.static("public"));
 
-app.use(express.static('public'))
-
-app.get('/', (req, resp) => {
-  resp.sendFile(path.join(__dirname, 'views/home.html'))
-})
+app.get("/", (req, resp) => {
+  resp.sendFile(path.join(__dirname, "views/home.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Server listens on port ${port}`)
-})
+  console.log(`Server listens on port ${port}`);
+});
